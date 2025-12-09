5d4a20b8fb6bb5f4a3a89219c642714090e66adc
jeannepurca <jeannemaemanicpurca@gmail.com>
2025-12-09 13:44:20 +0800

    Update (120925, 1:44PM)

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler(LOG_DIR / "flight.log"),
+        logging.FileHandler(config.LOG_DIR / "flight.log"),
         logging.StreamHandler()
     ]
 )
