236bcf6d81711bb76b539d72b9f41e10e9139091
jeannepurca <jeannemaemanicpurca@gmail.com>
2025-12-12 12:39:15 +0800

    Update testmavlink.py

--- testmavlink.py
+++ testmavlink.py
@@ -4,15 +4,14 @@ from mavsdk import System
 async def main():
     drone = System()
     await drone.connect("serial:///dev/ttyAMA0:57600")
+    
     print("Waiting for heartbeat...")
     async for state in drone.core.connection_state():
         if state.is_connected:
             print("Pixhawk connected!")
             break
 
-    # Set mission progress stream rate
-    await drone.telemetry.set_rate_mission_progress(2)
-
+    # Mission progress subscription (streaming)
     async for progress in drone.mission.mission_progress():
         print(f"Waypoint {progress.current}/{progress.total}")
 
