08abcb3a5098366be90221558b0ce650ff12013e
jeannepurca <jeannemaemanicpurca@gmail.com>
2025-12-12 17:22:14 +0800

    .

--- andeng.py
+++
@@ -1,8 +0,0 @@
-from pymavlink import mavutil
-
-master = mavutil.mavlink_connection('/dev/serial0', baud=57600)
-master.wait_heartbeat()
-master.mav.statustext_send(
-    mavutil.mavlink.MAV_SEVERITY_NOTICE,
-    b"TEST_MESSAGE"
-)
