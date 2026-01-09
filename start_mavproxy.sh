8961ec286ad0aacf763990a442bdf94fbc80d1e0
jeannepurca <jeannemaemanicpurca@gmail.com>
2026-01-10 00:15:37 +0800

    .

--- start_mavproxy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /home/ece4/PinyaSuri_ECE4
-source pinyasuri_env/bin/activate
-mavproxy.py --master=/dev/ttyAMA0 --baudrate=57600 \
-  --out=udp:127.0.0.1:14551 \
-  --daemon \
-  --state-basedir=/tmp/mavproxy_pinyasuri
